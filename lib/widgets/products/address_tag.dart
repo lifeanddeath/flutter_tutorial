f37b4ce7a328cb89678aef40733b1b287566fa9a
Ahmet Eroglu <aeroglu2016@gmail.com>
2019-04-09 10:48:22 +0200

    first commit

---
+++ lib/widgets/products/address_tag.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AddressTag extends StatelessWidget {
+  final String address;
+
+  AddressTag(this.address);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 5.0),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey, width: 1.0),
+          borderRadius: BorderRadius.circular(6.0)),
+      child: Text(address),
+    );
+  }
+}
---
+++ lib/widgets/products/product_card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/models/product.dart';
+
+import './price_tag.dart';
+import './ui_elements/title_default.dart';
+import './address_tag.dart';
+import '../../models/product.dart';
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+  final int productIndex;
+
+  ProductCard(this.product, this.productIndex);
+
+  Widget _buildTitleAndPriceRow() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TitleDefault(product.title),
+          SizedBox(
+            width: 8.0,
+          ),
+          PriceTag(product.price.toString())
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActionButtons(BuildContext context) {
+    return ButtonBar(
+      alignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.info,
+          ),
+          color: Theme.of(context).accentColor,
+          onPressed: () => Navigator.pushNamed<bool>(
+                context,
+                '/product/' + productIndex.toString(),
+              ),
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.favorite_border,
+          ),
+          color: Colors.red,
+          onPressed: () => Navigator.pushNamed<bool>(
+                context,
+                '/product/' + productIndex.toString(),
+              ),
+        )
+      ],
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset(product.image),
+          _buildTitleAndPriceRow(),
+          AddressTag('Union Square, San Fransisco'),
+          _buildActionButtons(context)
+        ],
+      ),
+    );
+  }
+}
